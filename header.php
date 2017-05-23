ee104cd396e56f371a8d701dedea0a3bc0ee1dad
juandarov <juandarov@gmail.com>
2017-05-23 14:56:40 +0200

    initial commit

---
+++ header.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <?php wp_head(); ?>
+    <title>Wordpress sundays</title>
+
+  </head>
+  <body>
+
+      <header>
+                  <nav class="navbar navbar-default">
+                      <div class="container-fluid">
+                          <div class="navbar-header">
+                              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                                  <span class="sr-only">Toggle navigation</span>
+                                  <span class="icon-bar"></span>
+                                  <span class="icon-bar"></span>
+                                  <span class="icon-bar"></span>
+                              </button>
+                          </div>
+                          <!-- Collect the nav links, forms, and other content for toggling -->
+                          <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                              <ul class="nav navbar-nav">
+                                  <li><a href="#">catalog</a></li>
+                                  <li><a href="aboutUs.html">about us</a></li>
+                                  <li><a href="#">stores</a></li>
+                                  <li><a href="#">blog</a></li>
+                              </ul>
+                          </div>
+                          <!-- /.navbar-collapse -->
+                      </div>
+                      <!-- /.container-fluid -->
+                  </nav>
+
+                  <div class="title">
+                      <h1>sundays</h1>
+                      <p>We craft ocean friendly apparel</p>
+                      <button class="feedback-button" type="button" name="button">browse our products</button>
+                  </div>
+              </header>
